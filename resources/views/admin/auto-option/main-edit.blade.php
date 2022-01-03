b9a8c268b99bbd7165e3344a25ca4d054d131e94
Yasin Siraj Shamrat <yshamrat@gmail.com>
2022-01-04 00:22:40 +0600

    option linking redesign

--- resources/views/admin/auto-option/main-edit.blade.php
+++ resources/views/admin/auto-option/main-edit.blade.php
@@ -37,7 +37,7 @@
                             @csrf
                             <label for="">{{ $option->name }}</label>
 
-                            <div class="form-group">
+                            {{-- <div class="form-group">
                                 <label for="">Main Option</label> <br>
                                 <select name="main_options[]" class="select2" multiple id="input-tags">
                                     <option value="0">Select Options</option>
@@ -45,7 +45,51 @@
                                         <option value="{{$item->id}}">{{$item->name}}</option>
                                     @endforeach
                                 </select>
-                            </div>
+                            </div> --}}
+							<table class="table table-bordered" id="mytable">
+								<thead>
+									<tr>
+										<td>Option</td>
+										<td align="center">
+											<button class="btn btn-sm btn-success" type="button" id="add_option"><i class="fa fa-plus"></i></button>
+										</td>
+									</tr>
+								</thead>
+								<tbody id="table_body">
+									@forelse ($option->autoMainOption as $i => $data)
+										<tr id="table_row_{{ $i }}">
+											<td>
+												<select name="main_options[]" class="w-100 form-control">
+													<option value="0">Select Option</option>
+													@foreach ($mainOptions as $item)
+														<option value="{{$item->id}}" @if ($data->id == $item->id)
+															selected
+														@endif>{{$item->name}}</option>
+													@endforeach
+												</select>
+											</td>
+											<td align="center">
+												<button class="btn btn-sm btn-danger" type="button" data-id="{{ $i }}" id="delete_option"><i class="fa fa-trash"></i></button>
+											</td>
+										</tr>
+									@empty
+										<tr id="table_row_1">
+											<td>
+												<select name="main_options[]" class="w-100 form-control">
+													<option value="0">Select Option</option>
+													@foreach ($mainOptions as $item)
+														<option value="{{$item->id}}">{{$item->name}}</option>
+													@endforeach
+												</select>
+											</td>
+											<td align="center">
+												<button class="btn btn-sm btn-danger" type="button" data-id="1" id="delete_option"><i class="fa fa-trash"></i></button>
+											</td>
+										</tr>
+									@endforelse
+
+								</tbody>
+							</table>
 							<div class="form-group">
 								<button class="btn btn-sm btn-warning text-right">Update</button>
 							</div>
@@ -91,22 +135,33 @@
 @section("script")
     <script>
         var values = {{ $option->autoMainOption->pluck('id') }};
-        $("#input-tags").select2();
-        $("#input-tags").val(values);
-        $("#input-tags").trigger('change');
+        // $("#input-tags").select2();
+        // $("#input-tags").val(values);
+        // $("#input-tags").trigger('change');
+		$(document).on("click","#add_option",function(){
+			var length = $("#mytable tr").length;
+			var row = `
+			<tr id="table_row_`+ (length - 1) +`">
+				<td>
+					<select name="main_options[]" class="w-100 form-control">
+						<option value="0">Select Option</option>
+						@foreach ($mainOptions as $item)
+							<option value="{{$item->id}}">{{$item->name}}</option>
+						@endforeach
+					</select>
+				</td>
+				<td align="center">
+					<button class="btn btn-sm btn-danger" type="button" data-id="`+ (length - 1) +`" id="delete_option"><i class="fa fa-trash"></i></button>
+				</td>
+			</tr>
+			`;
+			$('#table_body').append(row);
+		});
 
-        // $(document).ready(function(){
-            // $("#input-tags").selectize({
-            // delimiter: ",",
-            // persist: false,
-            // create: function (input) {
-            //     return {
-            //     value: input,
-            //     text: input,
-            //     };
-            // },
-            // });
-        // });
+		$(document).on("click","#delete_option",function(){
+			var id = $(this).data('id');
+			$("#table_row_"+id).remove();
+		});
 
     </script>
 @endsection
