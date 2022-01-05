46e539f08d4b48619e2b1c3e894350bba7d23a96
Yasin Siraj Shamrat <yshamrat@gmail.com>
2022-01-05 22:56:18 +0600

    Option Sorting fixed

--- resources/views/admin/auto-option/secondary-edit.blade.php
+++ resources/views/admin/auto-option/secondary-edit.blade.php
@@ -90,6 +90,15 @@
         $("#input-tags").val(values);
         $("#input-tags").trigger('change');
 
+		$("select").on("select2:select", function (evt) {
+			var element = evt.params.data.element;
+			var $element = $(element);
+
+			$element.detach();
+			$(this).append($element);
+			$(this).trigger("change");
+		});
+
         // $(document).ready(function(){
             // $("#input-tags").selectize({
             // delimiter: ",",
